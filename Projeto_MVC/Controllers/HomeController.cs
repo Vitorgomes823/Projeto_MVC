8272706daf9b9d1d6d4526c9d85e111f59f52978
vitor.silva <vitor.silva@BR-LAP-0726>
2025-03-17 09:51:16 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_MVC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Projeto_MVC.Models;
+
+namespace Projeto_MVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        HomeModel home = new HomeModel();
+        home.UserName = "Vitor";
+        home.UserEmail = "vitorgomessilva01@gmail.com";
+        return View(home);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Projeto_MVC/Models/HomeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Projeto_MVC.Models
+{
+    public class HomeModel
+    {
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public int UserCPF { get; set; }
+    }
+}
